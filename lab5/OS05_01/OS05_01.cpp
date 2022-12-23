44ecea5581591909c200c311e8724cde52299106
ksesha <72476438+crinanis@users.noreply.github.com>
2022-12-23 10:43:14 +0300

    Add files via upload

---
+++ lab5/OS05_01/OS05_01.cpp
@@ -0,0 +1,115 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+void printProcessPrty(HANDLE h)
+{
+    DWORD prty = GetPriorityClass(h);
+    cout << " ----- Current PID = " << GetCurrentProcessId() << " \n";
+    switch (prty)
+    {
+    case IDLE_PRIORITY_CLASS:
+        cout << " ----+ priority = IDLE_PRIORITY_CLASS \n";
+        break;
+    case BELOW_NORMAL_PRIORITY_CLASS:
+        cout << " ----+ priority = BELOW_NORMAL_PRIORITY_CLASS \n";
+        break;
+    case NORMAL_PRIORITY_CLASS:
+        cout << " ----+ priority = NORMAL_PRIORITY_CLASS \n";
+        break;
+    case ABOVE_NORMAL_PRIORITY_CLASS:
+        cout << " ----+ priority = ABOVE_NORMAL_PRIORITY_CLASS \n";
+        break;
+    case HIGH_PRIORITY_CLASS:
+        cout << " ----+ priority = HIGH_PRIORITY_CLASS \n";
+        break;
+    case REALTIME_PRIORITY_CLASS:
+        cout << " ----+ priority = REALTIME_PRIORITY_CLASS \n";
+        break;
+    default:
+        cout << " ----+ priority = ? \n";
+        break;
+    }
+    return;
+}
+
+void printThreadPrty(HANDLE h)
+{
+    DWORD icpu = SetThreadIdealProcessor(h, MAXIMUM_PROCESSORS);
+    DWORD prty = GetThreadPriority(h);
+    cout << " ---- Current Thread ID = " << GetCurrentThreadId() << "\n";
+    cout << " ----+ Processor = " << icpu << " \n";
+    //cout << " ----+ MAXProcessors = " << MAXIMUM_PROCESSORS << " \n";
+
+   
+
+    switch (prty)
+    {
+    case THREAD_PRIORITY_LOWEST:
+        cout << " ----+ priority = THREAD_PRIORITY_LOWEST \n";
+        break;
+    case THREAD_PRIORITY_BELOW_NORMAL:
+        cout << " ----+ priority = THREAD_PRIORITY_BELOW_NORMAL \n";
+        break;
+    case THREAD_PRIORITY_NORMAL:
+        cout << " ----+ priority = THREAD_PRIORITY_NORMAL \n";
+        break;
+    case THREAD_PRIORITY_ABOVE_NORMAL:
+        cout << " ----+ priority = THREAD_PRIORITY_ABOVE_NORMAL \n";
+        break;
+    case THREAD_PRIORITY_HIGHEST:
+        cout << " ----+ priority = THREAD_PRIORITY_HIGHEST \n";
+        break;
+    case THREAD_PRIORITY_IDLE:
+        cout << " ----+ priority = THREAD_PRIORITY_IDLE \n";
+        break;
+    case THREAD_PRIORITY_TIME_CRITICAL:
+        cout << " ----+ priority = THREAD_PRIORITY_TIME_CRITICAL \n";
+        break;
+    default:
+        cout << " ----+ priority = ? \n"; break;
+    }
+    return;
+}
+
+int main()
+{
+    HANDLE hp = GetCurrentProcess();
+    HANDLE ht = GetCurrentThread();
+
+    cout << " =====================OS05_01=======================\n";
+    try
+    {
+        {
+            DWORD pa = NULL, sa = NULL, icpu = -1;
+            if (!GetProcessAffinityMask(hp, &pa, &sa))
+                throw "GetProcessAffinityMask";
+
+            bitset<8> pa_bits(pa); //числа в двоичном формате (например, bin). Если необходим двоичный вывод 
+            //можно использовать трюк std::bitset:
+            cout << " Process Affinity Mask: " << std::showbase << std::hex << pa << "\n";
+            //showbase - Выводить индикатор основания   системы счисления
+            cout << " Process Affinity Mask: " << pa_bits << "\n";
+            bitset<8> sa_bits(sa);
+
+            cout << " System Affinity Mask: " << std::showbase << sa << "\n";
+            cout << " System Affinity Mask: " << sa_bits << "\n";
+            cout << " Processors: " << std::noshowbase << sa_bits.count() << "\n";
+        }
+    }
+    catch (char* msg)
+    {
+        cout << "Error " << msg << endl;
+    }
+    cout << dec << noshowbase;
+
+    printProcessPrty(hp);
+    printThreadPrty(ht);
+    cout << "\n\n";
+
+    cout << " =====================================================\n";
+    system("pause");
+    return 0;
+}
---
+++ lab5/OS05_02/OS05_02.cpp
@@ -0,0 +1,102 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <cassert>
+#include <sstream>
+
+using namespace std;
+
+void CreateProccersWithPriority(LPCWSTR an, STARTUPINFO si, int priority)
+{
+    PROCESS_INFORMATION pi;
+    if (priority == 1)
+    {
+        if (CreateProcess(an, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE | BELOW_NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi))
+            cout << "-- Process OS05_021 created\n";
+        else  cout << "-- Process OS05_021 not created\n";
+    }
+    if (priority == 2)
+    {
+        if (CreateProcess(an, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE | NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi))
+            cout << "-- Process OS05_021 created\n";
+        else  cout << "-- Process OS05_021 not created\n";
+    }
+    if (priority == 3)
+    {
+        if (CreateProcess(an, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE | HIGH_PRIORITY_CLASS, NULL, NULL, &si, &pi))
+            cout << "-- Process OS05_021 created\n";
+        else  cout << "-- Process OS05_021 not created\n";
+    }
+    if (priority == 4)
+    {
+        if (CreateProcess(an, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE | REALTIME_PRIORITY_CLASS, NULL, NULL, &si, &pi))
+            cout << "-- Process OS05_021 created\n";
+        else  cout << "-- Process OS05_021 not created\n";
+    }
+}
+
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "ru");
+
+    for (int i = 0; i < argc; i++)
+    {
+        cout << "arg" << i << ": " << argv[i] << endl;
+    }
+
+    HANDLE hp = GetCurrentProcess();
+
+    try
+    {
+        DWORD pa = NULL, sa = NULL, icpu = -1;
+
+        if (!GetProcessAffinityMask(hp, &pa, &sa))
+            throw "GetProcessAffinityMask";
+
+        cout << " Process Affinity Mask: " << showbase << hex << pa << "\n";
+        cout << " System Affinity Mask: " << showbase << hex << sa << "\n";
+        cout << dec << noshowbase << "\n\n";
+
+
+        if (argv[1] != NULL)
+        {
+            unsigned long long mask;
+
+            sscanf_s(argv[1], "%d", &mask);
+
+            if (!SetProcessAffinityMask(hp, mask))
+                throw "SetProcessAffinityMask";
+
+            if (!GetProcessAffinityMask(hp, &pa, &sa))
+                throw "GetProcessAffinityMask";
+
+            cout << " Process Affinity Mask: " << showbase << hex << pa << "\n";
+            cout << " System Affinity Mask: " << showbase << hex << sa << "\n";
+            cout << dec << noshowbase;
+
+            if (argv[2] != NULL && argv[3] != NULL)
+            {
+                STARTUPINFO si;
+
+                ZeroMemory(&si, sizeof(STARTUPINFO));
+                si.cb = sizeof(STARTUPINFO);
+
+                unsigned long long priority;
+                sscanf_s(argv[2], "%d", &priority);
+
+                CreateProccersWithPriority(L"d:\\1POIT\\3\\ОСиСП\\Лабы\\lab5\\Debug\\OS05_02x.exe", si, priority);
+
+                sscanf_s(argv[3], "%d", &priority);
+
+                ZeroMemory(&si, sizeof(STARTUPINFO));
+                si.cb = sizeof(STARTUPINFO);
+
+                CreateProccersWithPriority(L"d:\\1POIT\\3\\ОСиСП\\Лабы\\lab5\\Debug\\OS05_02x.exe", si, priority);
+            }
+        }
+    }
+    catch (char* msg)
+    {
+        cout << "Error " << msg << "\n";
+    }
+}
