ac4ad23e94b418995c29c53aa662b388212b4287
ksesha <72476438+crinanis@users.noreply.github.com>
2022-12-30 12:45:58 +0300

    Add files via upload

---
+++ Lab8/OS08_01/OS08_01/OS08_01.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    time_t now = time(0);
+    tm ltm;
+    localtime_s(&ltm, &now);
+    cout << "date and time: " << ltm.tm_mday << "." << ltm.tm_mon + 1 << "." << ltm.tm_year + 1900 << " " << ltm.tm_hour << ":" << ltm.tm_min << ":" << ltm.tm_sec << endl;
+}
