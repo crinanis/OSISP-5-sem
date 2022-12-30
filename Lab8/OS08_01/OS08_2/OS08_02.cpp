2c873d3b317462df35f6b42fd53fb3d133a37907
ksesha <72476438+crinanis@users.noreply.github.com>
2022-12-30 12:44:13 +0300

    Add files via upload

---
+++ Lab8/OS08_01/OS08_2/OS08_02.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    clock_t t1 = clock();
+    while (true)
+    {
+        i++;
+        clock_t t2 = clock();
+        if ((t2 - t1) == 5000 || (t2 - t1) == 10000)
+        {
+            cout << "number: " << i << endl;
+        }
+        if ((t2 - t1) == 15000) break;
+    }
+    cout << "end: " << i << endl;
+}
